3198e60b83ef5951bf5e22da21339bcdb09e9fc5
Mauri <mauri.monzon91@gmail.com>
2023-11-21 14:12:13 -0300

    Metadatos

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,14 @@
 import api from "@/api";
 
+export async function generateMetadata({params: {id}}: {params: {id: string}}) {
+  const restaurant = await api.fetch(id);
+
+  return {
+    title: `${restaurant.name} - Restaurancy`,
+    description: restaurant.description,
+  };
+}
+
 import RestaurantCard from "../components/RestaurantCard";
 
 export default async function RestaurantPage({params: {id}}: {params: {id: string}}) {
